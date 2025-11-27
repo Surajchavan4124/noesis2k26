2b698ab429d8df0f8e583e789f81ef368aafe395
Suraj Chavan <surajchavan4124@gmail.com>
2025-11-27 18:05:05 +0530

    add contact page

---
+++ js/coordinators.js
@@ -0,0 +1,100 @@
+const noesisCoordinators = [
+    {
+        id: "noesis-main-coordinator-1",
+        name: "Piyush",
+        role: "Noesis 2K26, Main Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush1",
+        phone_no: "+919848474893",
+        email: "piyush@noesis2k26.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-main-coordinator-2",
+        name: "Velron",
+        role: "Noesis 2K26, Assistant Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Velron1",
+        phone_no: "+919848474893",
+        email: "velron@noesis2k26.com",
+        whatsapp: "https://wa.me/919848474893"
+    }
+];
+
+const facultyCoordinators = [
+    {
+        id: "faculty-coordinator-1",
+        name: "Asst. Prof Sonia Marshal",
+        role: "Noesis 2k26, Event Convenor",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Sonia1",
+        phone_no: "+919848474893",
+        email: "sonia@rosarycollege.ac.in",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "faculty-coordinator-2",
+        name: "Asst. Prof Sonia Marshal",
+        role: "Noesis 2k26, Event Convenor",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Sonia2",
+        phone_no: "+919848474893",
+        email: "sonia@rosarycollege.ac.in",
+        whatsapp: "https://wa.me/919848474893"
+    }
+];
+
+const studentCoordinators = [
+    {
+        id: "noesis-coordinator-3",
+        name: "Piyush",
+        role: "Web Wizards Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush2",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-coordinator-4",
+        name: "Piyush",
+        role: "Media Crafter Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush3",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-coordinator-5",
+        name: "Piyush",
+        role: "Swing of Fortune Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush4",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-coordinator-6",
+        name: "Piyush",
+        role: "Code Crusader Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush5",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-coordinator-7",
+        name: "Piyush",
+        role: "Innoquest Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush6",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    },
+    {
+        id: "noesis-coordinator-8",
+        name: "Piyush",
+        role: "Gamera Groove Coordinator",
+        imageUrl: "https://api.dicebear.com/8.x/thumbs/svg?seed=Piyush7",
+        phone_no: "+919848474893",
+        email: "piyush@gmail.com",
+        whatsapp: "https://wa.me/919848474893"
+    }
+];
+
+
